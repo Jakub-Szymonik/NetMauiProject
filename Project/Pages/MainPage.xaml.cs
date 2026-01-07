5d7b3a521fc3719816745d20bce91620e491ac7e
Jakub-PC <Jakub-PC@DESKTOP-OHT5VNK>
2026-01-07 03:40:22 +0000

    Started the main page

---
+++ Project/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Project.Pages;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+}
