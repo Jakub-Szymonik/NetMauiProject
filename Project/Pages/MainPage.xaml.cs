7b9bd8aa93078927774c4b3f1b7f490ba4de9345
Jakub-PC <Jakub-PC@DESKTOP-OHT5VNK>
2026-01-07 04:53:46 +0000

    Started main page cs, time for a nap

--- Project/Pages/MainPage.xaml.cs
+++ Project/Pages/MainPage.xaml.cs
@@ -1,11 +1,18 @@
+using Project.Models;
+using Project.Services;
 namespace Project.Pages;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private DataService DataService;
+    private List<Movie> allMovies;
+    private UserData userData;
+    public MainPage()
+    {
+        InitializeComponent();
+        DataService = new DataService();
+        allMovies = new List<Movie>();
+    }
 
     private void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
